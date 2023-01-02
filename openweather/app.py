cf59aefdc5b5ac587352ff13ab1d4baacced51d2
oxydo <dantunezglez@gmail.com>
2023-01-02 19:27:07 +0100

    openweather module using openweather API

--- openweather/app.py
+++ openweather/app.py
@@ -1,7 +1,9 @@
 from theticketpost.application import Application
 from flask import render_template, request
+from loguru import logger
+import json
+import requests
 
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'svg', 'png'}
 
 class App(Application):
 
@@ -9,14 +11,29 @@ class App(Application):
         Application.__init__(self, __name__, __file__, desc)
 
 
+    def get_forecast(self):
+        # get app configuration parameters
+        response = self.get_configuration_json()
+        json_object = json.loads(response.data)
+        api_key = next((item for item in json_object if item["name"] == "api_key"), None)
+        lat = next((item for item in json_object if item["name"] == "lat"), None)
+        lon = next((item for item in json_object if item["name"] == "lon"), None)
+        unit = next((item for item in json_object if item["name"] == "unit"), None)
+
+        # request city name from lat & lon
+        url = "http://api.openweathermap.org/geo/1.0/reverse?lat=%s&lon=%s&limit=1&appid=%s" % (lat["value"], lon["value"], api_key["value"])
+        response = requests.get(url)
+        data = json.loads(response.text)
+        city_name = data[0]["name"]
+
+        # request data forecast to openweather
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=%s" % (lat["value"], lon["value"], api_key["value"], unit["value"])
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        return {"city_name": city_name, "current_temp": data["current"]["temp"]}
+
+
     def render_component(self):
-        #content_type = request.headers.get('Content-Type')
-        #if (content_type == 'application/json'):
-        #    config = request.json
-        #    img_filename = ""
-        #    for element in config:
-        #        if element["name"] == "variable_name":
-        #            variable_value = element["value"]
-        #            break
-        #     return render_template('openweather/component.html', variable_name=variable_value)
-        return render_template('openweather/component.html')
+        forecast = self.get_forecast()
+        return render_template('openweather/component.html', city_name = forecast["city_name"], current_temp=forecast["current_temp"])
