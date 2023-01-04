ddec3a1febc9c3f177593c5d61ad7296df1d4abc
oxydo <dantunezglez@gmail.com>
2023-01-04 21:12:50 +0100

    Fix on cookiecutter.json and sudoku module skeleton

---
+++ sudoku/app.py
@@ -0,0 +1,22 @@
+from theticketpost.application import Application
+from flask import render_template, request
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'svg', 'png'}
+
+class App(Application):
+
+    def __init__(self, desc):
+        Application.__init__(self, __name__, __file__, desc)
+
+
+    def render_component(self):
+        #content_type = request.headers.get('Content-Type')
+        #if (content_type == 'application/json'):
+        #    response = request.json
+        #    img_filename = ""
+        #    for element in response:
+        #        if element["name"] == "variable_name":
+        #            variable_value = element["value"]
+        #            break
+        #     return render_template('sudoku/component.html', variable_name=variable_value)
+        return render_template('sudoku/component.html')
