ab5c13da59d347e7b111fa7f27b5d47ddbd80b23
oxydo <dantunezglez@gmail.com>
2023-01-05 05:05:12 +0100

    Module to be used to separate by sections

---
+++ divider/app.py
@@ -0,0 +1,20 @@
+from theticketpost.application import Application
+from flask import render_template, request
+
+
+class App(Application):
+
+    def __init__(self, desc):
+        Application.__init__(self, __name__, __file__, desc)
+
+
+    def render_component(self):
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            response = request.json
+            img_filename = ""
+            for element in response:
+                if element["name"] == "text_to_print":
+                    text_to_print = element["value"]
+                    break
+            return render_template('divider/component.html', text_to_print=text_to_print )
