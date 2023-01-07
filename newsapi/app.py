5da0ee4f278d83e0329526fee55c35d70af7f5b0
oxydo <dantunezglez@gmail.com>
2023-01-07 12:44:32 +0100

    New module named newsapi

---
+++ newsapi/app.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(current)
+
+from theticketpost.application import Application
+from flask import render_template, request
+
+
+class App(Application):
+
+    def __init__(self, desc):
+        Application.__init__(self, __name__, __file__, desc)
+
+
+    def render_component(self):
+        #content_type = request.headers.get('Content-Type')
+        #if (content_type == 'application/json'):
+        #    response = request.json
+        #    img_filename = ""
+        #    for element in response:
+        #        if element["name"] == "variable_name":
+        #            variable_value = element["value"]
+        #            break
+        #     return render_template('newsapi/component.html', variable_name=variable_value)
+        return render_template('newsapi/component.html')
